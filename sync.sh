ed6f5059c343178e54fc48732180956a89b31c50
Toscano <lorenzo.toscano@gmail.com>
2018-01-24 14:26:48 +0100

    24012018

--- sync.sh
+++
@@ -1,4 +0,0 @@
-cp -r ./_site/* ../ltoscano.github.io/
-cd ..
-cd ltoscano.github.io
-grep -rli 'http:\/\/localhost:4000' * | xargs gsed -i 's/http:\/\/localhost:4000/https:\/\/ltoscano.github.com/g' 
